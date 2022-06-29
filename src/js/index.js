bd924efcc4274bb18da4895752d624c1fa0b5342
Iván Márquez García <ivanmarquezgarcia00@gmail.com>
2022-06-29 23:31:44 +0200

    initial version

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+function showHome() {
+    $($("#i_content")[0]).load("../html/home.html");
+}
+
+function showEducation() {
+    $($("#i_content")[0]).load("../html/education.html");
+}
+
+function showProjects() {
+    $($("#i_content")[0]).load("../html/working_section.html");
+}
+
+function showContact() {
+    $($("#i_content")[0]).load("../html/working_section.html");
+}
