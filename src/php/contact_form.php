ce125150bc51770569a1f500959398796d3049b0
Iván Márquez García <ivanmarquezgarcia00@gmail.com>
2022-06-30 22:00:51 +0200

    fix: show working section on contact and remove projects

--- src/php/contact_form.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if($_POST["message"]) {
-        mail(
-            "ivanmarquezgarcia00@gmail.com", "Portfolio Contact",
-            $_POST["message"]. "From: " . $_POST["email"]
-        );
-    }
-?>
