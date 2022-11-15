e21ca5eac56e45697182b1c0008d1261fe18fe29
IvanMarquezGarcia <ivanmarquezgarcia@protonmail.com>
2022-11-16 00:06:01 +0100

    feature: added section to tell about my jobs

--- src/js/index.js
+++ src/js/index.js
@@ -14,3 +14,16 @@ function showContact() {
     //$($("#i_content")[0]).load("src/html/working_section.html");
     $($("#i_content")[0]).load("src/html/contact.html");
 }
+
+function showJobHistory(from_education) {
+    //$($("#i_content")[0]).load("src/html/working_section.html");
+    $($("#i_content")[0]).load("src/html/job_history.html");
+
+    if (from_education == true) {
+        $($("#left-button-education")).attr("aria-selected", false);
+        $($("#left-button-education")).attr("class", "nav-link");
+    }
+    
+    $($("#left-button-job_history")).attr("aria-selected", true);
+    $($("#left-button-job_history")).attr("class", "nav-link active");
+}
